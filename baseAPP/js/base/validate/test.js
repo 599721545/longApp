bf4dc37b7c116f742845a985d299d92a8e9f9939
long <599721545@qq.com>
2018-04-03 08:48:24 +0800

    Signed-off-by: long <599721545@qq.com>

--- baseAPP/js/base/validate/test.js
+++ baseAPP/js/base/validate/test.js
@@ -4,5 +4,6 @@ dataValidate.test=function(obj,callback){
 	console.log("测试进入方法",obj);
 	if(callback&&typeof(callback)=="function"){
 		callback.call(this,obj,"回调测试");
+		console.log("dsads");
 	}
 }
